ff1a3659be3dc479c1bde9e158fd554299cb4554
RatKira <112857329+RatKira@users.noreply.github.com>
2022-09-05 02:45:48 -0600

    Update createPatchEmbed.js

--- src/scripts/createPatchEmbed.js
+++ src/scripts/createPatchEmbed.js
@@ -12,17 +12,4 @@ const currentPatchNotes = fs
     .replace(/^### (.*$)/gim, "**$1**")
     .replace(/^#### (.*$)/gim, "**$1**");
 
-const patchEmbed = new Discord.MessageEmbed()
-    .setColor("#66757f")
-    .setTitle(`${version} Release Notes`)
-    .setURL(releaseUrl)
-    .setDescription(currentPatchNotes)
-    .setThumbnail(
-        "https://raw.githubusercontent.com/RileyAbr/rat-facts-Discord-Bot/master/rat_logo.png"
-    )
-    .setFooter(
-        "rat facts created by Riley Abrahamson",
-        "https://avatars3.githubusercontent.com/u/32375220?s=460&u=a375968682555bb66226556bf7ee33d6d35b7961&v=4"
-    );
-
 exports.patchEmbed = patchEmbed;
