0638a6853e71a5b3049f95e451af4afa348cdfce
RatKira <112857329+RatKira@users.noreply.github.com>
2022-09-05 23:40:13 -0600

    Update joke.js

--- src/commands/main/joke.js
+++ src/commands/main/joke.js
@@ -2,7 +2,7 @@ const ratJokes = require("../../data/jokes");
 
 module.exports = {
     name: "joke",
-    commandUsage: "!rat joke",
+    commandUsage: "/rat joke",
     description: "Tells a joke about rats",
     execute(msg, args) {
         msg.channel.send(
