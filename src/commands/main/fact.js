5e9d5bb0edd141593e04605b08a9448f766fbab9
RatKira <112857329+RatKira@users.noreply.github.com>
2022-09-05 23:39:45 -0600

    Update fact.js

--- src/commands/main/fact.js
+++ src/commands/main/fact.js
@@ -2,7 +2,7 @@ const ratFacts = require("../../data/facts.json");
 
 module.exports = {
     name: "fact",
-    commandUsage: "!rat fact (!rat)",
+    commandUsage: "/rat fact (!rat)",
     description: "Gives a fact about rats",
     execute(msg, args) {
         msg.channel.send(
